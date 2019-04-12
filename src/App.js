37197e273b672a7175861602b24ff20099906e01
zmybie <2402368990@qq.com>
2019-04-12 10:18:52 +0800

    use antd

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+//引入antd样式
+import 'antd/dist/antd.css';
+
+import { Input } from 'antd';
+import { Button } from 'antd';
+import { List, Typography } from 'antd';
+
+const data = [
+  'Racing car sprays burning fuel into crowd.',
+  'Japanese princess to wed commoner.',
+  'Australian walks 100km after outback crash.',
+  'Man charged over missing wedding girl.',
+  'Los Angeles battles huge wildfires.',
+];
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div style={{margin:10}}>
+        <Input placeholder={'todo info'} style={{width:300,marginRight:10}}/>
+        <Button type="primary">提交</Button>
+        <List
+            style={{width:300}}
+            size="small"
+            bordered
+            dataSource={data}
+            renderItem={item => (<List.Item>{item}</List.Item>)}
+        />
       </div>
     );
   }
